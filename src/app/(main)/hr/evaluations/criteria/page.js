b531d7bf5175c29b86ed1337ffce5017dd56dd2c
hgchoi <hangy3ol@dystec.co.kr>
2025-09-04 15:20:17 +0900

    옵션 수정

--- src/app/(main)/hr/evaluations/criteria/page.js
+++ src/app/(main)/hr/evaluations/criteria/page.js
@@ -14,7 +14,7 @@ export default async function CriteriaPage() {
 
   // 필터옵션
   const [jobGroupResult, JobTitleResult] = await Promise.all([
-    fetchJobGroupOptions(),
+    fetchJobGroupOptions({ mode: 'evaluation' }),
     fetchJobTitleOptions(),
   ]);
 
